b3141d6e00a715448098cb5674148c9e8a975190
d.rudenko <ph.dmitry.rudenko@gmail.com>
2018-06-13 13:23:58 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,17 @@ module.exports = function (env) {
             filename: 'js/bundle.js'
         },
         module: {
-            rules: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+            rules:
+                [{
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
-                }
-            },
-                {
+                }, {
                     test: require.resolve('jquery'),
                     use: [{
                         loader: 'expose-loader',
@@ -28,31 +28,27 @@ module.exports = function (env) {
                         loader: 'expose-loader',
                         options: '$'
                     }]
-                },
-                {
-                    test: /\.scss$/,
+                }, {
+                    test: /\.s?css$/,
                     use: ExtractTextPlugin.extract({
                         filename: '[name].min.css',
                         fallback: 'style-loader',
-                        use: [{
-                            loader: 'css-loader',
-                            options: {url: false, minimize: true}
-                        },
-                            'sass-loader'
-                        ]
+                        use:
+                            [{
+                                loader: 'css-loader',
+                                options: {url: false, minimize: true}
+                            }, {
+                                loader: 'postcss-loader',
+                                options: {
+                                    plugins: [
+                                        autoprefixer({
+                                            browsers: ['last 4 version']
+                                        })
+                                    ],
+                                    sourceMap: true
+                                }
+                            }, 'sass-loader']
                     })
-                },
-                {
-                    test: /\.scss$/,
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins: [
-                            autoprefixer({
-                                browsers: ['ie >= 8', 'last 4 version']
-                            })
-                        ],
-                        sourceMap: true
-                    }
                 }]
         },
         stats: {
