da20e9f97e866597a0d4a6c460717597c94232c8
dmitry-rudenko <ph.dmitry.rudenko@gmail.com>
2019-06-13 11:48:28 +0300

    add vue.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,77 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-module.exports = function (env) {
-    return {
-        entry: './src/main.js',
-        output: {
-            path: __dirname + '/',
-            filename: 'js/bundle.js'
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports = function(env) {
+  return {
+    entry: './src/main.js',
+    output: {
+      path: __dirname + '/',
+      filename: 'js/bundle.js'
+    },
+    module: {
+      rules: [{
+          test: /\.vue$/,
+          loader: 'vue-loader'
         },
-        module: {
-            rules:
-                [{
-                    test: /\.js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                }, {
-                    test: require.resolve('jquery'),
-                    use: [{
-                        loader: 'expose-loader',
-                        options: 'jQuery'
-                    }, {
-                        loader: 'expose-loader',
-                        options: '$'
-                    }]
-                }, {
-                    test: /\.s?css$/,
-                    use: ExtractTextPlugin.extract({
-                        filename: '[name].min.css',
-                        fallback: 'style-loader',
-                        use:
-                            [{
-                                loader: 'css-loader',
-                                options: {url: true, import: true}
-                            }, {
-                                loader: 'postcss-loader',
-                                options: {
-                                    plugins: [
-                                        cssnano({
-                                            preset: 'default',
-                                        }),
-                                        autoprefixer({
-                                            browsers: ['last 4 version']
-                                        })
-                                    ],
-                                    sourceMap: true
-                                }
-                            }, 'sass-loader']
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }, {
+          test: require.resolve('jquery'),
+          use: [{
+            loader: 'expose-loader',
+            options: 'jQuery'
+          }, {
+            loader: 'expose-loader',
+            options: '$'
+          }]
+        }, {
+          test: /\.s?css$/,
+          use: ExtractTextPlugin.extract({
+            filename: '[name].min.css',
+            fallback: 'style-loader',
+            use: [{
+                loader: 'css-loader',
+                options: { url: true, import: true, sourceMap: true }
+              }, {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: [
+                    cssnano({
+                      preset: 'default',
+                    }),
+                    autoprefixer({
+                      browsers: ['last 4 version']
                     })
-                }]
-        },
-        stats: {
-            // Colored output
-            colors: true
-        },
-        plugins: [
-            new ExtractTextPlugin('css/main.min.css'),
-        ],
+                  ],
+                  sourceMap: true
+                }
+              }, {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    stats: {
+      // Colored output
+      colors: true
+    },
+    plugins: [
+      new ExtractTextPlugin('css/main.min.css'),
+      new VueLoaderPlugin()
+    ],
 
-    }
+  }
 };
